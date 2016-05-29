53a3bdb33bbf276ab4c6e7d77b04aba055591864
trevormast <trevormast@gmail.com>
2016-05-28 21:37:24 -0700

    Fixes nav for mobile

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -5,8 +5,22 @@ var height = $('.wrapper.style1.first').outerHeight(); //gets height of header
 $(window).scroll(function(){
     if($(window).scrollTop() > (topofDiv + height)){
        $(".logo").addClass('active');
+       addActiveNav();
     }
     else{
        $(".logo").removeClass('active');
+       removeActiveNav();
     }
 });
+
+function addActiveNav() {
+  if(screen.width <= 736) {
+    $('#menu-bar').addClass('active')
+  }
+}
+
+function removeActiveNav() {
+  if(screen.width <= 736) {
+    $('#menu-bar').removeClass('active')
+  }
+}
